458ceb420abc06319bcc005899299a8543e944f1
descellia <descelliiaa@gmail.com>
2025-09-05 17:01:58 +0700

    SS

--- app/src/main/java/com/example/lab_week_02_a/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_02_a/MainActivity.kt
@@ -22,4 +22,28 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+    override fun onRestart(){
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+    override fun onStart(){
+        super.onStart()
+        Log.d(TAG,"onStart")
+    }
+    override fun onResume(){
+        super.onResume()
+        Log.d(TAG,"onResume")
+    }
+    override fun onPause(){
+        super.onPause()
+        Log.d(TAG,"onPause")
+    }
+    override fun onStop(){
+        super.onStop()
+        Log.d(TAG,"onStop")
+    }
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.d(TAG,"onDestroy")
+    }
 }
